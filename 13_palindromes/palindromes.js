fed4ac48405ca0c632c6d561eebf97b0e9e4b4cd
huzzeyfh <hudzaifah7103@gmail.com>
2025-09-17 22:43:32 +0700

    Do exercise number 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const cleanedStr = str.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+    const reversedStr = cleanedStr.split('').reverse().join('');
 
+    const check = (cleanedStr === reversedStr) ? true : false;
+    return check;
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
