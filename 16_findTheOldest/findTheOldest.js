41cdb3a36d386fc6395f7059ba19ea4f34933ba1
huzzeyfh <hudzaifah7103@gmail.com>
2025-09-18 22:41:07 +0700

    Do the last exercise
    Huuraa

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,25 @@
-const findTheOldest = function() {
+const addDate = new Date();
+const propertyToCheck = 'yearOfDeath';
 
-};
+function findTheOldest(arr) {
+    arr.forEach(obj => {
+        if (!Object.hasOwn(obj, propertyToCheck)) {
+           obj.yearOfDeath = addDate.getFullYear();
+        }
+    });
 
+    const peopleAge = arr.map(({
+        name, yearOfBirth, yearOfDeath
+    }) => ({
+        name: name, age: yearOfDeath - yearOfBirth
+    }));
+
+
+    const filterAge = peopleAge.sort((a, b) => a.age - b.age);
+
+    return filterAge[filterAge.length - 1];
+}
+
+// console.log(findTheOldest(people));
 // Do not edit below this line
 module.exports = findTheOldest;
